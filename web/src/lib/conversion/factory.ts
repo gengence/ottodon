a42727d96f105227143e1e0e3dc2e5474eefb1ee
CLBL <150438866+cursedlifeblessedlife@users.noreply.github.com>
2025-05-19 15:38:32 -0500

    style: comment removals

--- web/src/lib/conversion/factory.ts
+++ web/src/lib/conversion/factory.ts
@@ -1,12 +1,12 @@
 import { MediaAdapter } from './types';
 import { ImageProcessor } from './adapters/imageAdapter';
 import { MediaProcessor } from './adapters/mediaAdapter';
-// import { DocumentProcessor } from './adapters/documentAdapter'; // Remove DocumentProcessor
+// import { DocumentProcessor } from './adapters/documentAdapter';
 
 const adapters: MediaAdapter[] = [
   new ImageProcessor(),
   new MediaProcessor(),
-  // new DocumentProcessor() // Remove DocumentProcessor instance
+  // new DocumentProcessor()
 ];
 
 export function getAdapter(mimeType: string): MediaAdapter | undefined {
